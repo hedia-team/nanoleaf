de41c009431a68461e2cdead35afb58fa0737f73
aimee rivers <aimee@aimeerivers.com>
2024-06-12 14:40:18 +0200

    fix: try to make it work on pi

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/pi/node-v20.13.1-linux-arm64/bin/node --watch /home/pi/nano/main.js
+/home/pi/node-v20.13.1-linux-arm64/bin/npm install && /home/pi/node-v20.13.1-linux-arm64/bin/npm run start
